78d7ad1146b0b3aea2e86e25aabed08fbc02c95b
michael_e_brown <michael_e_brown@dell.com>
2022-02-25 00:08:21 -0600

    untested but 'running' docker compose with alpine-based static compiled reference tools daemons, influx, mysql, grafana, activemq

---
+++ distribution/docker/alpine-individual/compose.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+scriptdir=$(cd $(dirname $0); pwd)
+topdir=$(cd $scriptdir/../../..; pwd)
+cd $topdir
+
+set -e
+
+# relies on already-built docker images. use 'build-docker.sh' to create if needed.
+echo ARGS: $@
+echo 1: $1
+
+echo "USER_ID=$(id -u)" > $topdir/.env
+echo "GROUP_ID=$(id -g)" >> $topdir/.env
+
+case $1 in
+  config)
+    docker-compose --project-dir $topdir -f $scriptdir/docker-compose.yml config
+    ;;
+
+  stop)
+    docker-compose --project-dir $topdir -f $scriptdir/docker-compose.yml rm -f
+    ;;
+
+  start)
+    docker-compose --project-dir $topdir -f $scriptdir/docker-compose.yml up --build --remove-orphans
+    ;;
+
+  *)
+    echo "Specify 'start' or 'stop' as the first argument"
+    exit 1
+    ;;
+esac
